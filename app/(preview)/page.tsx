0c1862daf8a7c0b43d30f645d35bb9878dbae127
vinatechpro <149946061+vinatechpro@users.noreply.github.com>
2024-11-26 23:00:31 +0700

    Update page.tsx

--- app/(preview)/page.tsx
+++ app/(preview)/page.tsx
@@ -21,6 +21,7 @@ import { Link } from "@/components/ui/link";
 import NextLink from "next/link";
 import { generateQuizTitle } from "./actions";
 import { AnimatePresence, motion } from "framer-motion";
+import { VercelIcon, GitIcon } from "@/components/icons";
 
 export default function ChatWithFiles() {
   const [files, setFiles] = useState<File[]>([]);
@@ -98,9 +99,9 @@ export default function ChatWithFiles() {
     setQuestions([]);
   };
 
-  const progress = partialQuestions ? (partialQuestions.length / 4) * 100 : 0;
+  const progress = partialQuestions ? (partialQuestions.length / 20) * 100 : 0;
 
-  if (questions.length === 4) {
+  if (questions.length === 20) {
     return (
       <Quiz title={title ?? "Quiz"} questions={questions} clearPDF={clearPDF} />
     );
@@ -221,7 +222,7 @@ export default function ChatWithFiles() {
                 />
                 <span className="text-muted-foreground text-center col-span-4 sm:col-span-2">
                   {partialQuestions
-                    ? `Generating question ${partialQuestions.length + 1} of 4`
+                    ? `Generating question ${partialQuestions.length + 1} of 20`
                     : "Analyzing PDF content"}
                 </span>
               </div>
@@ -229,7 +230,29 @@ export default function ChatWithFiles() {
           </CardFooter>
         )}
       </Card>
-      
+      <motion.div
+        className="flex flex-row gap-4 items-center justify-between fixed bottom-6 text-xs "
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <NextLink
+          target="_blank"
+          href="https://github.com/vercel-labs/ai-sdk-preview-pdf-support"
+          className="flex flex-row gap-2 items-center border px-2 py-1.5 rounded-md hover:bg-zinc-100 dark:border-zinc-800 dark:hover:bg-zinc-800"
+        >
+          <GitIcon />
+          View Source Code
+        </NextLink>
+
+        <NextLink
+          target="_blank"
+          href="https://vercel.com/templates/next.js/ai-quiz-generator"
+          className="flex flex-row gap-2 items-center bg-zinc-900 px-2 py-1.5 rounded-md text-zinc-50 hover:bg-zinc-950 dark:bg-zinc-100 dark:text-zinc-900 dark:hover:bg-zinc-50"
+        >
+          <VercelIcon size={14} />
+          Deploy with Vercel
+        </NextLink>
+      </motion.div>
     </div>
   );
 }
